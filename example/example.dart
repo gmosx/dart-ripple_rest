4b1ecbff0538376cd9c35da606d58880cbd71157
George Moschovitis <george.moschovitis@gmail.com>
2014-05-05 20:51:25 +0300

    Added more tests.

--- example/example.dart
+++ example/example.dart
@@ -3,20 +3,10 @@ import 'package:ripple_rest/ripple_rest_io.dart';
 void main() {
   final client = new RippleRestClient('http://192.168.0.14:5990');
 
-//  client.getBalances('rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh').then((balances) {
-//    balances.forEach((b) {
-//      print("${b.currency}/${b.counterparty}: ${b.value}");
-//    });
-//  });
-//
 //  client.getTrustlines('rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh').then((trustlines) {
 //    trustlines.forEach((t) {
 //      print("${t.currency}/${t.counterparty}: ${t.limit}");
 //    });
-//  });
-//
-//  client.getTransaction('6434F18B3997D81152F1AB31911E8D40E1346A05478419B7B3DF78B270C1151A').then((tx) {
-//    print(tx);
 //  });
 //
 //  client.getTransaction('126434F18B3997D81152F1AB31911E8D40E1346A05478419B7B3DF78B270C1151A').catchError(print);
--- test/ripple_rest_io_test.dart
+++ test/ripple_rest_io_test.dart
@@ -19,9 +19,19 @@ void main() {
     });
 
     test("provides access to account balances", () {
-      client.getBalances('rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh').then(expectAsync((balances) {
+      client.getBalances('rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh').then(expectAsync((List<Balance> balances) {
         expect(balances.length, greaterThan(0));
+        expect(balances.first.currency, equals('XRP'));
       }));
     });
+
+    test("provides access to transactions", () {
+      client.getTransaction('6434F18B3997D81152F1AB31911E8D40E1346A05478419B7B3DF78B270C1151A').then(expectAsync((tx) {
+        expect(tx['Fee'], equals("12"));
+        expect(tx['LimitAmount']['currency'], equals('USD'));
+      }));
+    });
+
+
   });
 }
